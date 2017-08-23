3a0a79436ada7903818bc02fe07ae479fdc40dbf
Joshua Raiser <joshua_raiser@hotmail.com>
2017-08-23 03:28:08 -0300

    Severas Modificações
    Comçando projeto novamente.

--- opencv-v3.2.0-vc14-app/opencv-v3.2.0-vc14/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : arquivo de origem que inclui apenas as inclusões padrões
-// opencv-v3.2.0-vc14.pch será o cabeçalho pré-compilado
-// stdafx.obj conterá as informações de tipo pré-compiladas
-
-#include "stdafx.h"
-
-// TODO: referencie qualquer cabeçalho adicional necessário em STDAFX.H
-// e não neste arquivo
