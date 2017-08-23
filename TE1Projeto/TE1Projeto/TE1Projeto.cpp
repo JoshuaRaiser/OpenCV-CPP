497c967cdb1b1ba815e46642a9d10f3f5519d81f
Joshua Raiser <joshua_raiser@hotmail.com>
2017-08-23 03:32:29 -0300

    PDI
    Adicionadas PDI.cpp e PDI.h

---
+++ TE1Projeto/TE1Projeto/TE1Projeto.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+#include "opencv2/opencv.hpp"
+#include "PDI.h"
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+	waitKey(0);
+
+	return 0;
+}
